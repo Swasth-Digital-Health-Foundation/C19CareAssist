ac5343329179e3e6a3db0309e63a15869b9cc731
Manzur Khan Sarguru <manzurkds@gmail.com>
2021-04-29 21:17:27 +0530

    feat: Setup Reminder Service - Get and decrypt users (#7)
    * feat: Setup Reminder Service - Get and decrypt users
    
    * feat: Send reminders to users
    
    * fix: Minor
    
    * fix: PR comments

---
+++ XState-Chatbot/nodejs/src/machine/messages/reminders.js
@@ -0,0 +1,13 @@
+let messages = {
+    morning: {
+        en_IN: `Good Morning! Hope you have your oximeter and thermometer handy. It is time to record your vitals and you can do that by typing “Hi” and going to “Manage my program” in the main menu.`
+    },
+    afternoon: {
+        en_IN: `Good Afternoon! Hope you have your oximeter and thermometer handy. It is time to record your vitals and you can do that by typing “Hi” and going to “Manage my program” in the main menu. `
+    },
+    evening: {
+        en_IN: `Good Evening! Hope you have your oximeter and thermometer handy. It is time to record your vitals and you can do that by typing “Hi” and going to “Manage my program” in the main menu.`
+    }
+  }
+  
+  module.exports.messages = messages;
---
+++ XState-Chatbot/nodejs/src/machine/notifications/reminders-service.js
@@ -0,0 +1,66 @@
+
+const { getQuery } = require('../service/util/api');
+const { personService } = require('../service/service-loader');
+const channelProvider = require('../../channel');
+const envVariables = require('../../env-variables');
+const { messages } = require('../messages/reminders');
+const dialog = require('../util/dialog.js');
+
+class RemindersService {
+
+    async triggerReminders(time) {
+      const people = await this.getSubscribedPeople();
+      this.sendMessages(people, time);
+    }
+
+    async getSubscribedPeople() {
+      //TODO: Get only unique mobile numbers, using mobile_hash
+        const query = `query GetSubscribedPeople($isSubscribed: Boolean) {
+            c19_triage(where: {subscribe: {_eq: $isSubscribed}}) {
+              person {
+                uuid
+                first_name
+                mobile
+              }
+            }
+          }
+          `
+
+        const variables = {
+            "isSubscribed": true
+          }
+        const data = await getQuery(query, variables, null);
+
+        const decrypedPeople = await this.decryptUserData(data.c19_triage);
+
+        const peopleWithUniqueMobileNumbers = this.filterDuplicateMobileNumbers(decrypedPeople);
+
+        return peopleWithUniqueMobileNumbers;
+    }
+
+    async decryptUserData(triageData) {
+      let people = triageData.map(item => item.person);
+      return await personService.decryptPersons(people);
+    }
+
+    filterDuplicateMobileNumbers(people) {
+      const mobileNumbers = people.map(person => person.mobile)
+      const peopleWithUniqueMobileNumbers = people.filter((person, index) => !mobileNumbers.includes(person.mobile, index + 1));
+      return peopleWithUniqueMobileNumbers
+    }
+
+    sendMessages(people, time) {
+        const extraInfo = {
+          whatsAppBusinessNumber: envVariables.whatsAppBusinessNumber
+        }
+
+        const message = dialog.get_message(messages[time]);
+
+        people.forEach(person => {
+          person.mobileNumber = person.mobile;
+          channelProvider.sendMessageToUser(person,[message],extraInfo)
+        });
+    }
+}
+
+module.exports = new RemindersService();
