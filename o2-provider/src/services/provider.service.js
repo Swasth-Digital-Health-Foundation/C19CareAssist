6cb479e66dd015f2a78799e5ceb92ebd9f42ff12
Rushang Dhanesha <rushangdhanesha@gmail.com>
2021-04-30 12:13:58 +0530

    Declared functions for providers and users

--- o2-provider/src/services/provider.service.js
+++ o2-provider/src/services/provider.service.js
@@ -1,6 +1,7 @@
 const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const logger = require('../config/logger');
+const { createUser } = require('./user.service');
 
 /**
  * Create a user
@@ -9,12 +10,28 @@ const logger = require('../config/logger');
  */
 const registerProvider = async (providerBody) => {
   logger.info(JSON.stringify(providerBody));
+  validateRegisterProvider(providerBody);
+  // create user
+  // persist provider
+
   if (false) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Provider mobile already taken');
   }
   // const user = await User.create(providerBody);
   return {};
 };
+
+const validateRegisterProvider = async (providerBody) => {
+
+}
+
+const validateUpdateProvider = async (providerBody) => {
+
+}
+
+const persistProvider = async (provider) => {
+  // 
+}
 
 module.exports = {
   registerProvider,
--- o2-provider/src/services/user.service.js
+++ o2-provider/src/services/user.service.js
@@ -2,3 +2,11 @@ const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const logger = require('../config/logger');
 
+const createUser = async (user) => {
+  // encrypt user
+  // persist user
+};
+
+module.exports = {
+  createUser
+};
