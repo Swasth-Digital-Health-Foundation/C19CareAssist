26c2028699e45686cf6185c73e576b3bc13533c4
Rushang Dhanesha <rushangdhanesha@gmail.com>
2021-05-04 11:41:35 +0530

    find matching providers based on pincode or city

---
+++ o2-provider/src/match/match-providers.js
@@ -0,0 +1,74 @@
+const { callHasura } = require('../services/util/hasura');
+
+const fetchPincodeMatchingProviders = async (location, iteration) => {
+  let { pin_code } = location;
+  let query;
+  let variables;
+  if (iteration === 0) {
+    query = `
+      query getO2Providers($pin_code: String!) {
+        o2_provider(where: {pin_code: {_eq: $pin_code}}) {
+          uuid
+          pin_code
+          city
+          o2_user{
+            mobile
+          }
+        }
+      }
+    `;
+  } else {
+    query = `
+      query getO2Providers($pin_code: String!) {
+        o2_provider(where: {pin_code: {_like: $pin_code}}) {
+          uuid
+          pin_code
+          city
+          o2_user{
+            mobile
+          }
+        }
+      }
+    `;
+    pin_code = pin_code.slice(0, -iteration);
+    pin_code += '%';
+  }
+
+  variables = {
+    pin_code,
+  };
+
+  const response = callHasura(query, variables, 'getO2Providers');
+  const data = response.data.o2_provider;
+
+  return data;
+};
+
+const fetchCityMatchingProviders = async (location, iteration) => {
+  if (iteration > 0) {
+    return [];
+  }
+  const { city } = location;
+  const query = `
+    query getO2Providers($city: String!) {
+      o2_provider(where: {city: {_eq: $city}}) {
+        uuid
+        pin_code
+        city
+      }
+    }
+  `;
+  const variables = {
+    city,
+  };
+
+  const response = callHasura(query, variables, 'getO2Providers');
+  const data = response.data.o2_provider;
+
+  return data;
+};
+
+module.exports = {
+  fetchPincodeMatchingProviders,
+  fetchCityMatchingProviders,
+};
