9c0fb4e74802666e5f64e14a6d90f429c9fffd19
Rushang Dhanesha <rushangdhanesha@gmail.com>
2021-05-05 21:58:01 +0530

    pincode based city matching

--- o2-provider/src/match/match-providers.js
+++ o2-provider/src/match/match-providers.js
@@ -50,7 +50,31 @@ const fetchCityMatchingProviders = async (location, iteration) => {
     city,
   };
 
-  const response = callHasura(query, variables, 'getO2Providers');
+  const response = await callHasura(query, variables, 'getO2Providers');
+  const data = response.data.o2_provider;
+
+  return data;
+};
+
+const fetchPincodeBasedCityMatchingProviders = async (location, iteration) => {
+  const query = `
+    query getO2Providers($find_pin: String!) {
+      o2_provider(where: {pin_code: {_like: $find_pin}, status: {_eq: "ACTIVE"}}) {
+        uuid
+        pin_code
+        city
+        o2_user {
+          mobile
+        }
+      }
+    }
+  `;
+  let { pin_code } = location;
+  pin_code = `${pin_code.substr(0, 2)}%`;
+  const variable = {
+    find_pin: pin_code,
+  };
+  const response = await callHasura(query, variable, 'getO2Providers');
   const data = response.data.o2_provider;
 
   return data;
@@ -59,4 +83,5 @@ const fetchCityMatchingProviders = async (location, iteration) => {
 module.exports = {
   fetchPincodeMatchingProviders,
   fetchCityMatchingProviders,
+  fetchPincodeBasedCityMatchingProviders,
 };
