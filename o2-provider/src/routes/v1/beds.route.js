358bd01b3c54bd2de4f0abd023f66fbe7ef87b2f
Vivek Srivastava <vivsri95@gmail.com>
2021-06-01 18:35:03 +0530

    fix: return response from download bed route.

--- o2-provider/src/routes/v1/beds.route.js
+++ o2-provider/src/routes/v1/beds.route.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const httpStatus = require('http-status');
 const { downloadSheet } = require('../../services/beds.services');
 
 const bedsRoute = express.Router();
-bedsRoute
-  .route('/download-sheet')
-  .post((req, res) => {
-    console.log('triggered download sheet');
-    downloadSheet();
-  });
+bedsRoute.route('/download-sheet').post((req, res) => {
+  console.log('triggered download sheet');
+  downloadSheet().then(() => res.status(httpStatus.OK).send(''));
+});
 
 module.exports = {
   bedsRoute,
