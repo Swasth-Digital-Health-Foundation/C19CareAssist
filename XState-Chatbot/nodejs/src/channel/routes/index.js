76b8a2a02667be9aeb49ecb8007239a469d1513a
Vivek Srivastava <vivsri95@gmail.com>
2021-06-10 21:55:56 +0530

    chore: add number in taskforce.

--- XState-Chatbot/nodejs/src/channel/routes/index.js
+++ XState-Chatbot/nodejs/src/channel/routes/index.js
@@ -1,31 +1,31 @@
 const express = require('express'),
-    router = express.Router(),
-    config = require('../../env-variables'),
-    sessionManager = require('../../session/session-manager'),
-    channelProvider = require('../');
-    remindersService = require('../../machine/notifications/reminders-service');
+  router = express.Router(),
+  config = require('../../env-variables'),
+  sessionManager = require('../../session/session-manager'),
+  channelProvider = require('../');
+remindersService = require('../../machine/notifications/reminders-service');
 
-router.post('/message', async (req, res) =>  {
-    processHttpRequest(req, res);
+router.post('/message', async (req, res) => {
+  processHttpRequest(req, res);
 });
 
-router.get('/message', async (req, res) =>  {
-    processHttpRequest(req, res);
+router.get('/message', async (req, res) => {
+  processHttpRequest(req, res);
 });
 
-router.post('/reminder', async (req, res) =>  {
+router.post('/reminder', async (req, res) => {
   remindersService.triggerReminders();
 });
 
+//TODO: add a cron job to fetch roles files.
 async function processHttpRequest(req, res) {
-    try {
-        let reformattedMessage = await channelProvider.processMessageFromUser(req);
-        if(reformattedMessage)
-            sessionManager.fromUser(reformattedMessage);
-    } catch(e) {
-        console.log(e);
-    }
-    res.end();
+  try {
+    let reformattedMessage = await channelProvider.processMessageFromUser(req);
+    if (reformattedMessage) sessionManager.fromUser(reformattedMessage);
+  } catch (e) {
+    console.log(e);
+  }
+  res.end();
 }
 
 router.get('/health', (req, res) => res.sendStatus(200));
