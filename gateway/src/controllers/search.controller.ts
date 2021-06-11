e20eab38d76f3aa791506d823a727afe42dc2794
Nithin <36623418+nithindv@users.noreply.github.com>
2021-06-11 11:06:26 +0530

    Revert "Feature/middleware for search api (#38)"
    This reverts commit 8cc7549adbd68845223f64b378f3edc30d0cf1ad.

--- gateway/src/controllers/search.controller.ts
+++ gateway/src/controllers/search.controller.ts
@@ -1,33 +1,31 @@
-import * as express from 'express';
-import { Request, Response, NextFunction } from 'express';
-import ControllerBase from '../interfaces/ControllerBase.interface';
-import { isAuthenticated } from '../middleware/auth';
-import { doctorConfirmation } from '../middleware/doctorConfirmation';
-import Search from '../services/search';
-import logger from '../utils/logger';
+import * as express from 'express'
+import { Request, Response } from 'express'
+import ControllerBase from '../interfaces/ControllerBase.interface'
+import { isAuthenticated } from '../middleware/auth' 
+import Search from '../services/search' 
+import logger from '../utils/logger'
 
 class SearchController implements ControllerBase {
-  public path = '/api/v1/search';
-  public router = express.Router();
+  public path = '/api/v1/search'
+  public router = express.Router()
 
   constructor() {
-    this.initRoutes();
+    this.initRoutes()
   }
 
   public initRoutes() {
-    this.router.post(`${this.path}/service`, isAuthenticated, this.search, doctorConfirmation);
+    this.router.post(`${this.path}/service`, isAuthenticated, this.search)
   }
 
-  private search = async (req: Request, res: Response, next: NextFunction) => {
+  search = async (req: Request, res: Response) => {
     try {
       const data: any = await new Search().getResults(req.body);
-      res.locals.doctorSearchUrl = data.services?.[0]?.provider?.api?.url;
-      next();
+      res.send(data).status(200);
     } catch (e) {
-      logger.error('Error in SearchController.search ', e);
-      res.status(500).send({ code: 500, message: e.message });
-    }
+      logger.error('Error in SearchController.search ', e)
+      res.status(500).send({ code: 500, message: e.message })
+    }    
   }
 }
 
-export default SearchController;
+export default SearchController
