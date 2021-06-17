23f7f8b60c7b78ccaf78ad9a2b57da6eb892db4c
yath-tw <yath.sharma@thoughtworks.com>
2021-06-17 15:03:54 +0100

    Tests for services

--- eua-service/src/tests/services/appointment.test.ts
+++ eua-service/src/tests/services/appointment.test.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import Search from '../../services/search';
+import Appointment from '../../services/appointment';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -7,14 +7,14 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 describe('getSearchResults()', () => {
   it('should return the appropriate response when the API call is successful', async () => {
     mockedAxios.request.mockResolvedValue({ data: {} });
-    const response = await new Search().getSearchResults({}, '');
+    const response = await new Appointment().getConfirmation('http://getappointmenturl', {});
     expect(response).toEqual({});
   });
 
   it('should throw the error when the API call is unsuccessful', async () => {
     mockedAxios.request.mockRejectedValue('The API call failed!');
     try {
-      await new Search().getSearchResults({}, '');
+      await new Appointment().getConfirmation('http://getappointmenturl', {});
       throw new Error('This test is not throwing the error as expected!');
     } catch (error) {
       expect(error).toEqual('The API call failed!');
