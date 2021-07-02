d4d09ce8ef3b34ffba5239b774a5863aa6e170f6
tarento-dharmateja <dharmateja.nandipattu@tarento.com>
2021-07-02 12:56:32 +0530

    Changes in API responses Structure

--- vitals-service/src/routes/patient.routes.js
+++ vitals-service/src/routes/patient.routes.js
@@ -3,8 +3,11 @@ const { isEmpty } = require("lodash");
 exports.patientAdd = async (req, res) => {
   try {
     let data = await patientService.createPerson(req.body);
-    if (isEmpty(data)) return res.sendStatus(409);
-    return res.send(data);
+      //TODO : found and Not found Move to config
+      let found = "1";
+      let notFound = "0";
+      if (isEmpty(data)) return res.status(200).send(notFound);
+      return res.send(found);
   } catch (err) {
 
   }
@@ -14,8 +17,11 @@ exports.patientSearch = async (req, res) => {
   try {
     if (req.query) {
       let data = await patientService.getPeople(req.query);
-      if (isEmpty(data)) return res.sendStatus(404);
-      return res.send(data);
+      //TODO : found and Not found Move to config
+      let found = "1";
+      let notFound = "0";
+      if (isEmpty(data)) return res.status(200).send(notFound);
+      return res.send(found);
     }
   } catch (err){
     
